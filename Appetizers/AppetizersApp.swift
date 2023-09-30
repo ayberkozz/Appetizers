7848e2e42d2de8030df111bead9dd1b86cfd7db4
Ayberk ÖZ <ayberkooz@hotmail.com>
2023-09-30 14:21:40 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ayberk Öz on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
